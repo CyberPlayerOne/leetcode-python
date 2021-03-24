1c5612a68a51c83c071b7b3456ce7047421a1c34
lonewar <lonewar@163.com>
2021-03-24 15:59:46 +1100

    #111 completed

--- 0111-Minimum-Depth-of-Binary-Tree/111.py
+++ 0111-Minimum-Depth-of-Binary-Tree/111.py
@@ -18,6 +18,21 @@ class Solution:
         q = deque([root])
         # root本身就是一层，depth初始化为1
         depth = 1
+
         while len(q) != 0:
-            sz = len(q)
-            pass  # TODO
+            sz = len(q)  # 二叉树中第depth层的节点个数
+            # 将当前队列中的所有节点向四周扩散
+            for i in range(sz):
+                cur = q.popleft()
+                # 判断是否到达终点
+                if cur.left is None and cur.right is None:
+                    return depth
+                # 将cur的相邻节点left和right加入队列
+                if cur.left is not None:
+                    q.append(cur.left)
+                if cur.right is not None:
+                    q.append(cur.right)
+            # 这里增加步数
+            depth += 1
+
+        return depth
