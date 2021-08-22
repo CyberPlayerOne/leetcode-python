7340f9bcada3bad2b11524c289f8a6245b5314a2
lonewar <lonewar@163.com>
2021-08-23 01:01:26 +1000

    #416

---
+++ 0416-Partition-Equal-Subset-Sum/416.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/partition-equal-subset-sum/
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        sum_ = sum(nums)
+        # 和为奇数时，不可能划分成两个和相等的集合
+        if sum_ % 2 != 0:
+            return False
+        n = len(nums)
+        sum_ = int(sum_ / 2)
+        dp = [[None for _ in range(sum_ + 1)] for _ in range(n + 1)]
+
+        # base case
+        for i in range(n + 1):
+            dp[i][0] = True
+
+        for i in range(1, n + 1):
+            for j in range(1, sum_ + 1):
+                if j - nums[i - 1] < 0:
+                    # 背包容量不足，不能装入第i个物品
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    # 装入或不装入背包
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+
+        return dp[n][sum_]
+
+
+nums = [1, 5, 11, 5]
+assert Solution().canPartition(nums) is True
