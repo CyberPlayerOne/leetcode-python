90f62c22cdaae391772122ac233e4ff6d7d0d41b
lonewar <lonewar@163.com>
2021-10-02 23:33:13 +1000

    ...

--- 0886-Possible-Bipartition/886.py
+++ 0886-Possible-Bipartition/886.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/possible-bipartition/
 # https://leetcode-cn.com/problems/possible-bipartition/
+from typing import List
+import collections
 
+
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
